096f2dc657fc18ee216129660913de2e7f7f8fe7
kamil Abdii <kamilabdii809@gmail.com>
2026-08-25 21:07:24 +0300

    Fix GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()
   ],
- base:"/kamil-e-commerce=coffee-shop/",
+ base:"/kamil-e-commerce-coffee-shop/",
 })
